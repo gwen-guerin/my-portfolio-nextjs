1afc2c62924d70969204d1d410ffc23ea6a96ac2
Gwen Guérin <gwenael.guerin@protonmail.com>
2023-02-07 10:28:45 +0100

    fixed bug

--- pages/myResume.jsx
+++ pages/myResume.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default function myResume() {
   return (
     <div className="flex justify-center items-center sm:w-full w-[90%] mb-14">
-      {/* <div className='sm:w-full w-[90%] mt-20 mb-14'> */}
-        <img className='ml-[10%] mt-20' src="assets/CV Gwenaël Guérin - DEV_page-0001.jpg" />
-      {/* </div> */}
+        <img className='sm:ml-0 ml-[10%] mt-20' src="assets/CV Gwenaël Guérin - DEV_page-0001.jpg" />
     </div>
   );
 }
